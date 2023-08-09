7f2a5b60719ba5a12ee51aecc2ffc5df60748c30
ejrtn0852 <135151859+ejrtn0852@users.noreply.github.com>
2023-08-09 19:23:40 +0900

    어지럽다
    객체저장왜안될까

--- javascript/login.js
+++ javascript/login.js
@@ -1,19 +1,49 @@
-const ID = document.getElementById('login-id');
+const userID = document.getElementById("login-id");
+const userPW = document.getElementById("login-pw");
+const loginForm = document.getElementById("login-input");
+const loginError = document.getElementById("login-error");
 
+let userIdCheck = false;
+let userPwCheck = false;
 
-const myId = {
-    id : "ejrtn0852@naver.com",
-    password : 1234
+// 몽고같은느낌
+const userObj = {
+    id: "ejrtn0852",
+    password : "1234"
 };
-  //  myId = 임시 계정;
 
-
-    function submitEvent (event) {
-        event.preventDefault();
-        console.log(document.getElementById('login-id').value);
+// 아이디가 유효하지 않을 시 boder에 빨간 줄 형성 시키는 기능.
+function redline () {
+    if (userIdCheck ){
+        userID.focus();
+        loginError.style.display = "flex";
+        userID.style.borderBottom = "1px solid tomato";
     }
+    if (userPwCheck) {
+        userPW.focus();
+        loginError.style.display = "flex";
+        userPW.style.borderBottom = "1px solid tomato";
+    }
+}
 
-document.getElementById('login-id').addEventListener("submit",submitEvent);
+function idError() {
+    if(userID.value !== userObj.id || !userID.value){
+        userIdCheck = true;
+        redline();
+    } 
+    if(userPW.value !== userObj.password || !userPW.value) {
+        userPwCheck = true;
+        redline();
+    } 
+    if(userID.value === userObj.id && userPW.value === userObj.password) {
+        loginForm.onsubmit = "return true";
+    }
+}
+
+
+
+
+loginForm.addEventListener('submit',idError);
 
 
 
