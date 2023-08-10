79be802123bd793b7fa25a52a657e34b91ae5272
ejrtn0852 <135151859+ejrtn0852@users.noreply.github.com>
2023-08-11 03:29:44 +0900

    시간 설정 미완
    갑분 호이스팅,함수변수 공부하느라 ㅋㅋ

--- utils or helpers/clock.js
+++ utils or helpers/clock.js
@@ -1,13 +1,13 @@
-const date = new Date();
-const hours = date.getHours();
-const min = date.getMinutes();
-const second = date.getSeconds();
+// const date = new Date();
+// const hours = date.getHours();
+// const min = date.getMinutes();
+// const second = date.getSeconds();
 
-const setHours = document.querySelector(".login-top-bar__clock > span:nth-child(1)")
-const setmin = document.querySelector(".login-top-bar__clock > span:nth-child(2)")
+// const setHours = document.querySelector(".login-top-bar__clock > span:nth-child(1)")
+// const setmin = document.querySelector(".login-top-bar__clock > span:nth-child(2)")
 
-const time = () => {
-    setHours.innerText = `${date.getSeconds()}`
-}
+// const time = () => {
+//     setHours.innerText = `${date.getSeconds()}`
+// }
 
-setInterval(time,1000);
+// setInterval(time,1000);
