a4928957a0743620a09a691dd99a6105e6ecabb9
ejrtn0852 <135151859+ejrtn0852@users.noreply.github.com>
2023-08-12 12:12:01 +0900

    시간설정 else처리까지 완

--- utils or helpers/clock.js
+++ utils or helpers/clock.js
@@ -4,9 +4,9 @@ const setHours = document.querySelector(".login-top-bar__clock > span");
 const time = function() {
     const date = new Date();
     const hours = date.getHours();
-    if(hours > 12) hours%12;
     const ampm = hours > 12 ? "PM" : "AM";
     if(date.getMinutes() < 10) setHours.innerText = `${hours}:0${date.getMinutes()}${ampm}`
+    else setHours.innerText = `${hours}:${date.getMinutes()}${ampm}`
 }
 
 setInterval(time,1000); 
